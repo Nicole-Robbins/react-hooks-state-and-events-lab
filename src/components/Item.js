a161dd93ba4bff6b6b03020514f18105eb12d312
Nicole Robbins <nicole.robbins@att.net>
2023-02-03 20:57:44 -0600

    commit tree 4bc62e29ab2b7cff684b41709437e768dee9f765

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isIn, setIsIn] = useState(true)
+  const  cartStatus = isIn ? "Add to Cart" : "Remove from Cart"
+  const listname = isIn ? "" : "in-cart"
+  function addToCart(){
+     setIsIn((isIn) => !isIn)
+  }
+  
   return (
-    <li className="">
+    <li className={listname}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addToCart}>{cartStatus}</button>
     </li>
   );
 }
